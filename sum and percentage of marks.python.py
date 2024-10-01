a9720ade11d1f8fae003ca2a3bcd21fabfe6e441
Basim-Shebin <basimshebin@gmail.com>
2024-10-01 11:59:32 +0530

    Update sum and percentage of marks.python.py

--- sum and percentage of marks.python.py
+++ sum and percentage of marks.python.py
@@ -1,8 +1,4 @@
-##write a python program to calculate total marks and percentage of 5 subjects##
-##write a pyhon program  to swapthe values without using temperory variables##
-##write a program tom input a number then increment the number by 5 and decrement the number by 3##
-
-print("Enter the marks out of 20")
+print("Enter the marks out of 500")
 a=int(input("Enter the marks 1 "))
 b=int(input("Enter the marks 2 "))
 c=int(input("Enter the marks 3 "))
@@ -10,6 +6,6 @@ d=int(input("Enter the marks 4 "))
 e=int(input("Enter the marks 5 "))
 
 s=a+b+c+d+e
-Per=(s/100)*100
+Per=(s/500)*100
 print("Total Marks Obtained = ",s)
 print("Percentage = ",Per,"%")
