5ffe00c88dfff39957c03bff4ade0a8df8c9d3e4
Basim-Shebin <basimshebin@gmail.com>
2024-10-23 12:06:16 +0530

    Delete week 4/occurance-of-word.py

--- week 4/occurance-of-word.py
+++
@@ -1,9 +0,0 @@
-string =input("Enter a line of text: ")
-w=input("enter the word ")
-a=[]
-count=0
-a=string.split(" ")
-for i in range(0,len(a)):
-    if(w==a[i]):
-        count=count+1
-print("count of word is : ",count)
