37792ef03cedebd352bafb43e3523016960d95d9
Basim-Shebin <basimshebin@gmail.com>
2024-10-23 11:40:25 +0530

    Update max,min and sort in list.py

--- week 4/max,min and sort in list.py
+++ week 4/max,min and sort in list.py
@@ -3,13 +3,13 @@ b=int(input("enter the size "))
 for i in range(b):
     c=int(input("enter the values "))
     a.append(c)
-
 print("The given list is ",a)
 max=max(a)
 min=min(a)
-asc=sorted(a)
 print("max value of list is",max)
 print("min value of list is",min)
-print("Sorted list is ",asc)
-asc.reverse()
-print("reverse sorted list is ",asc)
+a.sort()
+print("The sorted list is ",a)
+a.reverse()
+print("The reverse of the list is ",a)
+
