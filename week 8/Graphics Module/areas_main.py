9382acf6be1d573fca40ff2356215c04e2bcd558
Basim-Shebin <basimshebin@gmail.com>
2024-12-12 14:46:02 +0530

    Add files via upload

---
+++ week 8/Graphics Module/areas_main.py
@@ -0,0 +1,9 @@
+from Graphics import circle
+from Graphics import rectangle
+from Graphics.Dgraphics import Cuboid
+from Graphics.Dgraphics import sphere
+
+print("area of rectangle (l=10,w=5)",rectangle.area(10,5))
+print("area of circle (r=7)",circle.area(7))
+print("area of sphere (r=7)",sphere.area(7))
+print("area of cuboid (l=10,w=5,h=6)",Cuboid.area(10,5,6))
